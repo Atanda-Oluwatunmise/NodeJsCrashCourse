466fafefdab14e3f12bc9d3f5b2f244b4136bb14
Tunmise Atanda <75289869+Atanda-Oluwatunmise@users.noreply.github.com>
2024-05-22 12:06:40 +0100

    loading files

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT;
+
+//Get current path
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+
 //creating a server
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     //creating a router
-    if(req.url === '/'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>Homepage</h1>');
-    } else if (req.url === '/about'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>About</h1>');
-    }else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end('<h1>Not Found</h1>');
+    try{
+        //Check if GET request
+        if(req.method === 'GET'){
+            let filepath;
+            if(req.url === '/'){
+                filepath = path.join(__dirname, 'public', 'index.html');
+            } else if (req.url === '/about'){
+                filepath = path.join(__dirname, 'public', 'about.html');
+            }else {
+                throw new Error('Not found');
+            }
+           
+            const data = await fs.readFile(filepath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
+        } else {
+            throw new Error('Method not allowed');
+        }
+    } catch (error){
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Server Error');
     }
-    console.log(req.url);
-    console.log(req.method);
 
 
 });
