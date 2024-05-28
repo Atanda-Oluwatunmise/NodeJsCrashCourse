c752890d4ce0a1b3da5c42ed066b410c06c212ef
Tunmise Atanda <75289869+Atanda-Oluwatunmise@users.noreply.github.com>
2024-05-28 12:41:29 +0100

    Tests the Os module

---
+++ osDemo.js
@@ -0,0 +1,14 @@
+import os from 'os';
+
+//basically giving information about your system
+//userInfo()
+console.log(os.userInfo().username);
+
+//totalmem()
+console.log(os.totalmem());
+
+//freemem()
+console.log(os.freemem())
+
+//cpus
+console.log(os.cpus())
